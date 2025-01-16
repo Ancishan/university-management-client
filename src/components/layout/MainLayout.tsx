b822d665659e4b2ddc20442324339eb1a1d1bb41
Ancishan <shantodev027@gmail.com>
2025-01-16 12:05:39 +0800

    [DONE] mainLayOut

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,16 +1,23 @@
-import { Layout, Menu } from "antd";
-import React from "react";
-import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import { Layout, Menu, MenuProps } from "antd";
 const { Header, Content, Footer, Sider } = Layout;
 const MainLayout = () => {
 
-    const items = [UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
-        (icon, index) => ({
-          key: String(index + 1),
-          icon: React.createElement(icon),
-          label: `nav ${index + 1}`,
-        }),
-      );
+    const items : MenuProps['items'] = [
+      {
+        key: '1',
+        label: 'DashBoard',
+        children:[
+          {
+            key:'11',
+            label:'Admin'
+          }
+        ]
+      },
+      {
+        key: '2',
+        label: 'Profile'
+      }
+    ]
 
   return (
     <Layout style={{height: '100vh'}}>
@@ -24,6 +31,7 @@ const MainLayout = () => {
           console.log(collapsed, type);
         }}
       >
+        <div style={{color: 'white', textAlign: 'center', height:'4rem', display: 'flex', justifyContent:'center'}}>University Management</div>
         <div className="demo-logo-vertical" />
         <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
       </Sider>
